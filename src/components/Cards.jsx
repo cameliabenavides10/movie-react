44cbc8784c5fefc999b8baff1bc20f382d31ccd0
cameliabenavides10 <cameliabenavides@gmail.com>
2023-05-30 17:09:20 -0500

    new api

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,37 +1,53 @@
+import { useState } from 'react';
 
 
-export default function Cards() {
+
+
+export default function Cards({ movie, url }) {
+
+
+    console.log(url);
     return (
         <>
-        
-                <div className="d-inline-flex picture-cards m-2 picture-cards" >
-                    <img src="https://images.unsplash.com/photo-1685300488141-76bc6bbd17d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1Nnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="img-fluid" alt="Responsive image" style={{ height: "300px", width: "300px" }} />
-                </div>
-                <div className="d-inline-flex picture-cards m-2" >
-                    <img src="https://images.unsplash.com/photo-1685391691429-d1a2e354cc42?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0MXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="img-fluid" alt="Responsive image" style={{ height: "300px", width: "300px" }} />
-                </div>
-                <div className="d-inline-flex picture-cards m-2" >
-                    <img src="https://images.unsplash.com/photo-1685306314640-31a02b996d1f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1NHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="img-fluid" alt="Responsive image" style={{ height: "300px", width: "300px" }} />
-                </div>
-                <div className="d-inline-flex picture-cards m-2" >
-                    <img src="https://images.unsplash.com/photo-1685268586501-ae3c3ca67efe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1Mnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="img-fluid" alt="Responsive image" style={{ height: "300px", width: "300px" }} />
-                </div>
-                <div className="d-inline-flex picture-cards m-2" >
-                    <img src="https://images.unsplash.com/photo-1685288532492-932a4e567279?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1OHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="img-fluid" alt="Responsive image" style={{ height: "300px", width: "300px" }} />
-                </div>
-                <div className="d-inline-flex picture-cards m-2" >
-                    <img src="https://images.unsplash.com/photo-1684019608073-e79bc1642ec5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3Nnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="img-fluid" alt="Responsive image" style={{ height: "300px", width: "300px" }} />
-                </div>
-                <div className="d-inline-flex picture-cards m-2" >
-                    <img src="https://images.unsplash.com/photo-1685287417745-0ed40cb5b3c1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4N3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="img-fluid" alt="Responsive image" style={{ height: "300px", width: "300px" }} />
-                </div>
-                <div className="d-inline-flex picture-cards m-2" >
-                    <img src="https://images.unsplash.com/photo-1685361063181-1d0471c063a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3OHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="img-fluid" alt="Responsive image" style={{ height: "300px", width: "300px" }} />
-                </div>
-                <div className="d-inline-flex picture-cards m-2" >
-                    <img src="https://images.unsplash.com/photo-1685345324402-460a889064d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4Mnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="img-fluid" alt="Responsive image" style={{ height: "300px", width: "300px" }} />
-                </div>
-            
+            {/* {movie.map(movie => {
+                return (
+                    <div key={movie.id} className="d-inline-flex picture-cards m-2 picture-cards" >
+
+                        <div >
+
+                            <div className="mb-4">
+                                {movie.originalTitleText.text}
+                            </div>
+                        </div>
+                    </div>
+                )
+
+            }
+            )} */}
+            {url.map((url) => {
+                return (
+                    <>
+                        {url && url.url ?
+                            (<div className='container'> 
+                                <div className="row d-inline-flex picture-cards m-2 picture-cards"> 
+                            <img src={url.url}
+
+                                class="img-fluid" alt="Responsive image" style={{ height: "270px", width: "300px" }} /> 
+                          <div className="row">      
+{url.caption.plainText}
+</div>        
+                                </div>
+                                </div>):
+                            (<p>"no pic</p>)
+                        }
+                        
+                    </>
+                )
+            }
+            )}
+
+
+
         </>
     )
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
 import axios from 'axios'
+import Cards from './Cards';
+
+
+
+
+
 export default function Search() {
     // our rerendering search term letters
     const [movieSearch, setMovieSearch] = useState('');
     console.log(movieSearch);
     // the rerendering title which is used in the fetching 
     const [movieTitle, setMovieTitle] = useState('');
+    // movie data variable
+    const [movie, setMovie] = useState([])
 
     // setting the movie title function 
     function handleSubmit(e) {
@@ -14,33 +22,31 @@ export default function Search() {
         setMovieSearch('');
     }
     console.log(movieTitle);
+    // using axios to fetch movie api data
+    const getMovie = async () => {
 
-// axios fetch movie API
-const getMovie = async() => {
-  const options = {
-    method: 'GET',
-    url: 'https://moviesdatabase.p.rapidapi.com/titles',
-    headers: {
-      'X-RapidAPI-Key': '23188ebbdcmsh7aeb3fccfa1534cp172313jsnbcc398cc377f',
-      'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
-    }
-  };
-  
-  try {
-      const response = await axios.request(options);
-      const movieData= response.data.results;
-      const ids = movieData.map(movie => movie.id)
-      console.log(movieData);
-      console.log(ids)
-  } catch (error) {
-      console.error(error);
-  }
-}
+        const options = {
+          method: 'GET',
+          url: 'https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=true&language=en-US&page=1&sort_by=popularity.desc',
+          headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NDhlMzMxZGM4NmI3MjlhZmI0ZjVmNzYzYThkNjRmNiIsInN1YiI6IjY0NzY3MDM1MjU1ZGJhMDEwODQxMDkwNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Vg_NToRT58vYoUWqG25XsMzVKEJINL0GmLdiO6P5C1w'
+          }
+        };
+        
+       try{
+          const response= await axios.request(options);
+          { console.log(response.data); }
+        } catch (error) {
+            console.error(error);
+          }
+        }
 
 
 
 
     return (
+        <>  
         <form
             type="submit"
             onSubmit={handleSubmit}>
@@ -56,11 +62,13 @@ const getMovie = async() => {
                 />
             </div>
             <button
-            onClick={getMovie}
+                onClick={getMovie}
                 type="submit"
                 class="btn btn-primary">
                 Submit
             </button>
         </form>
+      {/* <Cards movie={movie} url={{url}.url}/> */}
+        </> 
     )
-}
+    }
